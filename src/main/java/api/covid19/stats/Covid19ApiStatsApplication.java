8788dd048e6879418f1f119c15532b3900dc8c84
Andrew.Nikitin <abn.dev.01@gmail.com>
2023-02-02 01:05:22 +0300

    start

---
+++ src/main/java/api/covid19/stats/Covid19ApiStatsApplication.java
@@ -0,0 +1,13 @@
+package api.covid19.stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19ApiStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19ApiStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/covid19/stats/Covid19ApiStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package api.covid19.stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19ApiStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

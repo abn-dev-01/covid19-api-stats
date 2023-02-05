ff57c2a043465f44ef1ccbcc1c1f31be50f1d907
Andrew.Nikitin <abn.dev.01@gmail.com>
2023-02-06 00:40:17 +0300

    h2 does not work

---
+++ src/main/resources/db/migration/V002.001.0003__table_total_country_indexes.sql
@@ -0,0 +1,5 @@
+CREATE INDEX IDX_COUNTRY_CODE ON "PUBLIC_SCHEMA"."entity_total_country"(country_code);
+
+CREATE INDEX IDX_COUNTRY ON "PUBLIC_SCHEMA"."entity_total_country"(country);
+
+CREATE INDEX IDX_DATE_STAT ON "PUBLIC_SCHEMA"."entity_total_country"(date_stat);
